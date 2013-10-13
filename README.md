e97be9cbe1592371f4dd9c850cbacdcbd87e6ba4
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-10-13 17:06:23 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+[imgur Upload](http://www.pinceladasdaweb.com.br/blog/uploads/imgur/)
+=================
+
+Upload images to imgur via JavaScript.
+
+##Important Note
+
+On line 63 of the file app.js add the Client-ID of your app. You can pick it hem: [http://api.imgur.com/](http://api.imgur.com/).
+
+##Browser Support
+
+* IE10+
+* Opera
+* Google Chrome
+* Safari
+* Firefox
---
+++ assets/js/app.js
@@ -0,0 +1,88 @@
+var Imgur = (function (d) {
+    "use strict";
+    /*jslint browser: true*/
+    var module = {
+        xhr: function () {
+            return new XMLHttpRequest();
+        },
+        create: function (name, props) {
+            var el = d.createElement(name), p;
+            for (p in props) {
+                if (props.hasOwnProperty(p)) {
+                    el[p] = props[p];
+                }
+            }
+            return el;
+        },
+        remove: function (els) {
+            while (els.hasChildNodes()) {
+                els.removeChild(els.lastChild);
+            }
+        },
+        bindEvent: function () {
+            var fileinput = d.querySelector('#uploadBtn'),
+                fileName  = d.querySelector('#uploadFile'),
+                status    = d.querySelector('.status'),
+                self      = this;
+
+            fileinput.addEventListener('change', function (e) {
+                var files = e.target.files, file, p, t, i, len;
+                for (i = 0, len = files.length; i < len; i += 1) {
+                    file = files[i];
+                    if (file.type.match(/image.*/)) {
+                        self.remove(status);
+                        fileName.value = this.value;
+
+                        p = self.create('p');
+                        t = d.createTextNode("Uploading...");
+
+                        p.appendChild(t);
+                        status.appendChild(p);
+
+                        self.upload(file);
+                    } else {
+                        self.remove(status);
+
+                        p = self.create('p');
+                        t = d.createTextNode("Invalid Archive");
+
+                        p.appendChild(t);
+                        status.appendChild(p);
+                    }
+                }
+            }, false);
+        },
+        upload: function (file) {
+            var xhttp    = module.xhr(),
+                status   = d.querySelector('.status'),
+                self     = this,
+                fd       = new FormData();
+
+            fd.append('image', file);
+            xhttp.open('POST', 'https://api.imgur.com/3/image');
+            xhttp.setRequestHeader('Authorization', 'Client-ID XXXXXXXXXXXXX'); //Get yout Client ID here: http://api.imgur.com/
+            xhttp.onreadystatechange = function () {
+                if (xhttp.status === 200 && xhttp.readyState === 4) {
+                    var res = JSON.parse(xhttp.responseText), link, p, t;
+
+                    self.remove(status);
+
+                    link = res.data.link;
+                    p    = self.create('p');
+                    t    = d.createTextNode(link);
+
+                    p.appendChild(t);
+                    status.appendChild(p);
+                }
+            };
+            xhttp.send(fd);
+        },
+        init: function () {
+            module.bindEvent();
+        }
+    };
+
+    return {
+        init: module.init
+    };
+}(document));
