454788eecd0a005b568949116cc29953b4841794
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-10-24 14:50:03 -0200

    Refatored imgur library

--- src/imgur.js
+++ src/imgur.js
@@ -1,116 +1,112 @@
-;(function(root, factory){
-    
+/*jslint browser: true, debug: true*/
+/*global define, module, exports*/
+(function (root, factory) {
+    "use strict";
     if (typeof define === 'function' && define.amd) {
-        define( factory );
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Imgur = factory();
     }
-    else if ( typeof exports === 'object' ) {
-        exports = module.exports = factory();
-    }
-    else {
-        root.Module = ( root.Module || {} );
-        root.Module.Imgur = factory();
-    }
+}(this, function () {
+    "use strict";
+    var Imgur = function (options) {
+        if (!this || !(this instanceof Imgur)) {
+            return new Imgur(options);
+        }
 
-})( this, function(){
-    /*
-    --------------------------------
-    imgur Upload
-    --------------------------------
-    + https://github.com/pinceladasdaweb/imgur-upload
-    + version 1.0
-    + Copyright 2013 Pedro Rogerio
-    + Licensed under the MIT license
+        if (!options) {
+            options = {};
+        }
 
-    + Documentation: https://github.com/pinceladasdaweb/imgur-upload
-    */
+        if (!options.clientid) {
+            throw 'Provide a valid Client Id here: http://api.imgur.com/';
+        }
 
-    function Imgur() {
-        "use strict";
-        /*jslint browser: true*/
-        var module = {
-            xhr: function () {
-                return new XMLHttpRequest();
-            },
-            create: function (name, props) {
-                var el = document.createElement(name), p;
-                for (p in props) {
-                    if (props.hasOwnProperty(p)) {
-                        el[p] = props[p];
+        this.clientid = options.clientid;
+        this.endpoint = 'https://api.imgur.com/3/image';
+        this.callback = options.callback || function () {};
+        this.dropzone = document.querySelectorAll('.dropzone');
+
+        this.run();
+    };
+
+    Imgur.prototype = {
+        createEls: function (name, props, text) {
+            var el = document.createElement(name), p;
+            for (p in props) {
+                if (props.hasOwnProperty(p)) {
+                    el[p] = props[p];
+                }
+            }
+            if (text) {
+                el.appendChild(document.createTextNode(text));
+            }
+            return el;
+        },
+        createDragZone: function () {
+            var p, input;
+
+            p     = this.createEls('p', {}, 'Drag your files here or click in this area.');
+            input = this.createEls('input', {type: 'file', accept: 'image/*'});
+
+            Array.prototype.forEach.call(this.dropzone, function (zone) {
+                zone.appendChild(p);
+                zone.appendChild(input);
+                this.upload(zone);
+            }.bind(this));
+        },
+        post: function (path, data, callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.open('POST', path, true);
+            xhttp.setRequestHeader('Authorization', 'Client-ID ' + this.clientid);
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status >= 200 && this.status < 400) {
+                        var response = '';
+                        try {
+                            response = JSON.parse(this.responseText);
+                        } catch (err) {
+                            response = this.responseText;
+                        }
+                        callback.call(window, response);
+                    } else {
+                        throw new Error(this.status + " - " + this.statusText);
                     }
                 }
-                return el;
-            },
-            remove: function (els) {
-                while (els.hasChildNodes()) {
-                    els.removeChild(els.lastChild);
-                }
-            },
-            bindEvent: function () {
-                var fileinput = document.querySelector('#uploadBtn'),
-                    fileName  = document.querySelector('#uploadFile'),
-                    status    = document.querySelector('.status'),
-                    self      = this;
+            };
+            xhttp.send(data);
+            xhttp = null;
+        },
+        upload: function (zone) {
+            var file, formData, target, i, len;
 
-                fileinput.addEventListener('change', function (e) {
-                    var files = e.target.files, file, p, t, i, len;
-                    for (i = 0, len = files.length; i < len; i += 1) {
-                        file = files[i];
-                        if (file.type.match(/image.*/)) {
-                            self.remove(status);
-                            fileName.value = this.value;
+            zone.addEventListener('change', function (e) {
+                if (e.target && e.target.nodeName === 'INPUT') {
+                    target = e.target.files;
 
-                            p = self.create('p');
-                            t = document.createTextNode("Uploading...");
+                    for (i = 0, len = target.length; i < len; i += 1) {
+                        file = target[i];
+                        if (file.type.match(/image/)) {
+                            formData = new FormData();
+                            formData.append('image', file);
 
-                            p.appendChild(t);
-                            status.appendChild(p);
-
-                            self.upload(file);
+                            this.post(this.endpoint, formData, function (data) {
+                                this.callback(data);
+                            }.bind(this));
                         } else {
-                            self.remove(status);
 
-                            p = self.create('p');
-                            t = document.createTextNode("Invalid Archive");
-
-                            p.appendChild(t);
-                            status.appendChild(p);
                         }
                     }
-                }, false);
-            },
-            upload: function (file) {
-                var self     = this,
-                    xhttp    = self.xhr(),
-                    status   = document.querySelector('.status'),
-                    fd       = new FormData();
+                }
+            }.bind(this), false);
+        },
+        run: function () {
+            this.createDragZone();
+        }
+    };
 
-                fd.append('image', file);
-                xhttp.open('POST', 'https://api.imgur.com/3/image');
-                xhttp.setRequestHeader('Authorization', 'Client-ID XXXXXXXXXXXXX'); //Get yout Client ID here: http://api.imgur.com/
-                xhttp.onreadystatechange = function () {
-                    if (xhttp.status === 200 && xhttp.readyState === 4) {
-                        var res = JSON.parse(xhttp.responseText), link, p, t;
-
-                        self.remove(status);
-
-                        link = res.data.link;
-                        p    = self.create('p');
-                        t    = document.createTextNode(link);
-
-                        p.appendChild(t);
-                        status.appendChild(p);
-                    }
-                };
-                xhttp.send(fd);
-            },
-            init: function () {
-                module.bindEvent();
-            }
-        };
-
-        return module;
-    }
-
-    window.Imgur = Imgur().init;
-    return Imgur();
-});
+    return Imgur;
+}));
